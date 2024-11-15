ccd90e538f3771ebcf3f5ca3a96d55c64d74bdb6
xzc <xuzhichao1618@qq.com>
2024-11-15 15:59:51 +0800

    feat: 测试

--- nest-server/src/app.controller.ts
+++ nest-server/src/app.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Version } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// 对整个控制器进行版本控制
+// @Controller({
+//   path: 'api',
+//   version: '1', // 此时接口变为 /v1/api/xxx
+//   // ... other options
+// })
 @Controller('/api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  // 也可以对某个接口进行版本控制
   @Get('/get')
+  @Version('1') // 此时接口变为 /v1/api/get
   getHello(): string {
     return this.appService.getHello();
   }
--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
-
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 版本控制
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.use(
+    session({
+      secret: 'xxxx',
+      name: 'xc.session', // 生成客户端cookie的名称
+      rolling: true, // 每次请求都更新cookie
+      cookie: { maxAge: null }, // 设置cookie的过期时间
+    }),
+  );
+  await app.listen(3000);
 }
 bootstrap();
